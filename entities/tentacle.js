307779f12ebf155a8293f8d94c09bf41d55657e6
Dale Price <daprice@mac.com>
2015-08-22 13:16:53 -0500

    Play eldritch music of doom with your eldritch appendages of doom

--- entities/tentacle.js
+++ entities/tentacle.js
@@ -1,10 +1,11 @@
 //the tentacular terror
 function Tentacle(game, x, y, side, note) {
 	this.count = Math.random() * 360;
-	this.note = note + 1;
+	this.note = note;
 	this.amplitude = Math.random() * 6 + 3;
 	this.game = game;
 	this.side = side;
+	this.prevActive = false;
 	
 	this.shown = false;
 	
@@ -23,11 +24,25 @@ function Tentacle(game, x, y, side, note) {
 	this.r = this.game.add.rope(x, y, 'tentacle', null, this.points);
 }
 
-Tentacle.prototype.update = function() {
-	this.count += this.note / 2 * 0.08 + 0.07;
-	
-	for (var i = 0; i < this.points.length; i++)
-    {
-        this.points[i].y = Math.sin(i * 0.5 + this.count) * this.amplitude * (10-i);
-    }
+Tentacle.prototype.update = function(active) {
+	if(active) { //if this tentacle is playing a note
+		this.count += (this.note+1) / 2 * 0.08 + 0.07;
+		
+		for (var i = 0; i < this.points.length; i++)
+	    {
+	        this.points[i].y = Math.sin(i * 0.5 + this.count) * this.amplitude * (10-i);
+	    }
+	    
+	    //play sound
+	    if(evilsoundready && !this.prevActive) {
+		    evilsounds[this.note].loopFull();
+		    console.log('playing note ' + this.note);
+	    }
+	    
+	    this.prevActive = true;
+	}
+	else { //if this tentacle is not playing a note
+		evilsounds[this.note].stop();
+		this.prevActive = false;
+	}
 };
